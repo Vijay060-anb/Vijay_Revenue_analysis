4530846b0516ad081576f9041a629b442db16d57
Vijay060-anb <vijayanbarasu06@gmail.com>
2026-07-16 21:13:34 +0200

    n8n files

---
+++ Microsoft SQL/Inventory and product performance analysis.sql
@@ -0,0 +1,19 @@
+SELECT TOP (5)
+    category,
+    inventory_pressure,
+    state,
+    discount_percent,
+    sales_event,
+    brand_type
+FROM dbo.Sales_data
+ORDER BY
+    CASE inventory_pressure
+        WHEN 'High' THEN 1
+        WHEN 'Low' THEN 2
+        ELSE 3
+    END,
+    CASE brand_type
+        WHEN 'Premium' THEN 1
+        WHEN 'Mass' THEN 2
+        ELSE 3
+    END;
